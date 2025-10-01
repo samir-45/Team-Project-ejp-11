1427552481bd66b4663d4de8b42e695e0f98ff44
samir-45 <mdmahinkhan621@gmail.com>
2025-10-01 13:35:26 +0600

    Simple text

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, This is out team project!</h1>
+      <h1 className="text-3xl font-bold underline">Hello, I am test brunch!</h1>
     </div>
   );
 }
