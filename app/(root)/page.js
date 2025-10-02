dd2d5ed1a8d2fd5b9754ab8f981a268006d8bcac
Fahim68555 <programhero541@gmail.com>
2025-10-02 23:54:12 +0600

    connected mongoDB

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,13 +1,9 @@
 import HeroSection from "@/components/(root)/HeroSection";
-import Nav from "@/components/(root)/nav";
-import Test from "@/components/Test";
-
 
 export default function Home() {
   return (
     <div>
       <HeroSection></HeroSection>
-      <Test></Test>
     </div>
   );
 }
