7c6afd8a1e0786b016c257ff95fc232da851a168
samir-45 <mdmahinkhan621@gmail.com>
2025-10-08 11:24:24 +0600

    Dashboard switching done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "./contexts/AppContext";
+import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AppProvider>
+          <SessionProvider>
           {children}
+          </SessionProvider>
         </AppProvider>
 
       </body>
